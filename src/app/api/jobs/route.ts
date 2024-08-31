a10fb14fae4c69155290f69f9af45e8d7ac3cc49
daniel-lou18 <danielderudder@hotmail.com>
2024-08-31 11:28:51 +0200

    Modify next api route and fetch json file from GitHub Pages

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -1,10 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import data from "../../../../public/data/data.json";
 import { filterJobs } from "@/utils/helpers";
+import axios from "@/services/axiosClient";
+import { Job } from "@/types";
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const query = searchParams.get("q");
-  const result = query ? filterJobs(data, query) : data;
-  return NextResponse.json(result, { status: 200 });
+  try {
+    const response = await axios.get<Job[]>(
+      "https://daniel-lou18.github.io/job-app/public/data/data.json",
+    );
+    const result = query ? filterJobs(response.data, query) : response.data;
+    return NextResponse.json(result, { status: 200 });
+  } catch (error: unknown) {
+    return NextResponse.json(
+      { message: error instanceof Error ? error.message : "Erreur inconnue" },
+      { status: 500 },
+    );
+  }
 }
