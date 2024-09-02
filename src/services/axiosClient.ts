e4bcb7005c277a81c47919e3d9ff0502a54ed0e7
daniel-lou18 <danielderudder@hotmail.com>
2024-09-02 16:01:38 +0200

    Add check for NODE_ENV in axiosClient

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -5,7 +5,9 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL:
-    "/" || process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+    process.env.NODE_ENV === "production"
+      ? "/"
+      : process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -7,7 +7,7 @@ import { handleServiceError } from "./handleServiceError";
 
 const jobService = (apiClient: IApiClient) => ({
   getJobs: async (searchParams?: URLSearchParams): Promise<Job[]> => {
-    console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
+    console.log(process.env.NODE_ENV);
     // Simulate the fetching delay
     await new Promise((resolve) => setTimeout(resolve, 750));
 
