7daa65497eb6b15851b17f2a0cbb23e395808f4b
daniel-lou18 <danielderudder@hotmail.com>
2024-09-17 15:31:06 +0200

    Wrap main component in Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Search from "@/components/search";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <Search />
+      <Suspense>
+        <Search />
+      </Suspense>
     </main>
   );
 }
